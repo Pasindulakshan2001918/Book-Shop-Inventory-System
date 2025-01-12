3ec4bbf4ebe5ea398d4c705b796f7cf814e12147
Pasindu <eg245157@engug.ruh.ac.lk>
2025-01-12 15:58:18 +0530

    update the navbar section

--- web/frontend/src/components/NavBar/NavBar.jsx
+++ web/frontend/src/components/NavBar/NavBar.jsx
@@ -1,16 +1,33 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { HiMiniBars3 } from "react-icons/hi2";
+import { FaRegUser } from "react-icons/fa6";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { GiShoppingCart } from "react-icons/gi";
 import { IoSearchOutline } from "react-icons/io5";
-import React from 'react'
 import './NavBar.css';
+import avatarImg from "../../assets/avatar.png"
+
+const navigation = [
+  {name: "Dashboard", href:"/user-dashboard"},
+  {name: "Orders", href:"/orders"},
+  {name: "Cart Page", href:"/cart"},
+  {name: "Check Out", href:"/checkout"},
+]
+
 
 const NavBar = () => {
+
+  const  [isDropdownOpen, setIsDropdownOpen] = useState(false)
+  console.log(isDropdownOpen)
+
+  const currentUser = true;
   return (
     <header className="navbar-container">
       <nav className="nav-flex">
         <div className="logo-search">
         <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
-          <HiMiniBars3 size={25} style={{ marginTop: '0px' }} />
+          <HiMiniBars3 size={25} style={{ marginTop: '0px', color: 'black' }} />
         </Link>
 
 
@@ -29,8 +46,54 @@ const NavBar = () => {
           </div>
         </div>
 
-        <div className="nav-items">
-          nav items
+        <div className="nav-items" >
+          <div>
+            {
+              currentUser ? <>
+              <button className="avatar-button" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+
+                <img src={avatarImg}  alt="" />
+
+              </button>
+              {/* show Dropdowns*/}
+              {
+                isDropdownOpen &&(
+
+                  <div className="item-list">
+                    <ul className="list-linespace">
+                      {
+                        navigation.map((item) =>(
+                          <li key={item.name} onClick={() =>
+                            setIsDropdownOpen(false)
+                          }>
+
+                            <Link to={item.href} className="list-style">
+
+                              {item.name}
+                            
+                            </Link>
+
+                          </li>
+                        ))
+                      }
+                    </ul>
+                  </div>
+
+                )
+              }
+
+              </> : <Link to="/ login"><FaRegUser size={20}  /></Link>
+            }
+          </div>
+        
+        <button className="heart-button">
+        <IoMdHeartEmpty size={25}/>
+        </button>
+
+        <Link to="/cart" className="cart-icon">
+        <GiShoppingCart size={19} style={{ color: 'black' }}/>
+        <span className="span" >0</span>
+        </Link>
         </div>
       </nav>
     </header>
